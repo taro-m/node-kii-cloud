716fed87b40d0ea330cfae751ac09d12977c4c5d
MURAOKA Taro <taro.muraoka@muraoka-design.com>
2013-07-05 17:23:02 +0900

    write JSON request framework

--- main.js
+++ main.js
@@ -2,10 +2,12 @@
 
 var config = require('./config.json');
 var kii = require('./lib/kii');
+var util = require('util');
 
 var ktx = new kii.Context(config.region, config.appID, config.appKey);
 ktx.setupAccessToken(config.clientID, config.clientSecret, handleAccessToken);
 
 function handleAccessToken(token) {
   console.log('HERE: ' + token);
+  console.log('ktx: ' + util.inspect(ktx));
 }
