63b383290f773f058beb86d8ad7137cca3e9270e
MURAOKA Taro <taro.muraoka@muraoka-design.com>
2013-07-05 17:08:46 +0900

    rename ctx to ktx

--- main.js
+++ main.js
@@ -3,8 +3,8 @@
 var config = require('./config.json');
 var kii = require('./lib/kii');
 
-var ctx = new kii.Context(config.region, config.appID, config.appKey);
-ctx.setupAccessToken(config.clientID, config.clientSecret, handleAccessToken);
+var ktx = new kii.Context(config.region, config.appID, config.appKey);
+ktx.setupAccessToken(config.clientID, config.clientSecret, handleAccessToken);
 
 function handleAccessToken(token) {
   console.log('HERE: ' + token);
